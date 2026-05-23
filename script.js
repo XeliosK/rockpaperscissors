8896c9c601748cc1bd10052e8d25c7ede7f75290
XeliosK <283260516+XeliosK@users.noreply.github.com>
2026-05-23 15:10:15 -0400

    Add playRound, Score functions

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-//COMPUTER TO SELECT CHOICE OF ROCK, PAPER, SCISSORS
-//REQUEST INPUT FROM USER ON ROCK, PAPER, SCISSORS
-//IF INPUT = ROCK & PC = PAPER, LOSE
-//IF INPUT = ROCK & PC = SCISSORS, WIN
-//IF INPUT = ROCK & PC = PAPER, TIE
-//REPEAT FOR INPUT PAPER = AND INPUT SCISSORS =
+// COMPUTER TO SELECT CHOICE OF ROCK, PAPER, SCISSORS
+// REQUEST INPUT FROM USER ON ROCK, PAPER, SCISSORS
+// RECORD WIN, LOSS, TIE WITH MESSAGE KEEPING SCORE PER ATTEMPT
+// IF INPUT = ROCK & PC = PAPER, LOSE
+// IF INPUT = ROCK & PC = SCISSORS, WIN
+// IF INPUT = ROCK & PC = PAPER, TIE
+// REPEAT FOR INPUT PAPER = AND INPUT SCISSORS =
 // IF WIN, DISPLAY MESSAGE WIN
 // IF LOSE, DISPLAY MESSAGE LOSE
 // IF TIE, DISPLAY MESSAGE TIE
+// CREATE GAME LOOP
 
 
-//FIRST WRITE COMPUTER CHOICE FUNCTION
+
+// INITIAL SCORE COUNTER
+
+const humanScore = 0;
+const computerScore = 0;
+
+// WRITE COMPUTER CHOICE FUNCTION
 
 function getComputerChoice () {
     const value = Math.random() * 100;
     if (value <= 33) {
-        return "Rock";
+        return "rock";
     } else if (value >= 33 & value <= 66) {
-        return "Paper";
+        return "raper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 // console.log (getComputerChoice()); // CHECKS GETCOMPUTERCHOICE FUNCTION
+
+// HUMAN CHOICE FUNCTION
+function getHumanChoice () {
+    const choice = prompt("Please select: rock, paper or scissors");
+    console.log("User entered: " + choice);
+}
+
+getHumanChoice();
+
+// PLAY ROUND FUNCTION
+
+function playRound (humanChoice, computerChoice) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+    if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log ("You chose: " + humanChoice + " PC chose: " + computerChoice)
+        console.log ("You win! Paper beats rock.")
+        return humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "rock") {
+        console.log ("You chose: " + humanChoice + " PC chose: " + computerChoice)
+        console.log ("It's a tie!")
+    } else {
+        console.log ("You chose: " + humanChoice + " PC chose: " + computerChoice)
+        console.log ("You lose! Rock beats scissors.")
+        return computerScore++;
+    }
+}
